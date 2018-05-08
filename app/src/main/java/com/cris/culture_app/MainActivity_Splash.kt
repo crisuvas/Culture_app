9ffc32c3316c31004d99a59fda265554c4b53ffa
Cris <crisuvas@gmail.com>
2018-05-07 20:08:37 -0500

    Initial commit

---
+++ app/src/main/java/com/cris/culture_app/MainActivity_Splash.kt
@@ -0,0 +1,12 @@
+package com.cris.culture_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main__splash)
+    }
+}
